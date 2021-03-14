4f6043c9ecf3ba7d3511de56e74318c440732434
Jaye Hackett <jaye.hackett@gmail.com>
2021-03-14 19:28:23 +0000

    fix(perf): clean up sass imports more

--- lbh/objects/objects.test.js
+++ lbh/objects/objects.test.js
@@ -1,13 +1,17 @@
-/* eslint-env jest */
-
-const glob = require("glob");
 const util = require("util");
 const sass = require("node-sass");
 
 const sassRender = util.promisify(sass.render);
 
-const sassFiles = glob.sync(`lbh/objects/**/*.scss`);
-
-it.each(sassFiles)("%s renders to CSS without errors", (file) => {
-  return sassRender({ file: file });
+describe("The objects layer", () => {
+  it("renders to CSS without errors", async () => {
+    return sassRender({
+      data: `
+      @import "base";
+      @import "objects/all";
+    `,
+      includePaths: ["lbh/"],
+      outputStyle: "nested",
+    });
+  });
 });
